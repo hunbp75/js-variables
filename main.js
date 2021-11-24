00ba0a919e12582ed82e21c0fe4fe96a1b5c6d22
hunbp75 <hunbp75@gmail.com>
2021-11-24 12:47:47 +0100

    :tada:

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let products = [
      {category: "konyhai",
       name: "turmix2000",
       price: 5000,
-      stock: 10, 
+      stock: 4, 
       attachements: ["fej", "motor"]
     },
      {category: "tisztálkodószer",
@@ -20,6 +20,8 @@ let products = [
     }
 ];
 
+let customer ="Ádám";
+
 function main() {
     console.log("It works");
     let product = products[2];
@@ -33,12 +35,44 @@ function main() {
 }
 
 function inventory() {
+
     let stock = products[2].stock;
-    let price = products[2].price;
+    let price = products[2].price; 
     let sumValue = stock * price;
     let div = document.createElement("div");
     div.innerText = products[2].name + " : " + sumValue + " Ft" ;
     document.body.append(div);
+
+    stock = products[0].stock;
+    price = products[0].price;
+    sumValue = stock * price;
+    div = document.createElement("div");
+    div.innerText = products[0].name + " : " + sumValue + " Ft" ;
+    document.body.append(div);
+
+    stock = products[1].stock;
+    price = products[1].price;
+    sumValue = stock * price;
+    div = document.createElement("div");
+    div.innerText = products[1].name + " : " + sumValue + " Ft" ;
+    document.body.append(div);   
+    
+    stock = products[0].stock;
+    let available = stock >= 5;
+    div = document.createElement("div");
+    if (available) {
+        div.innerText = products[0].name + "elérhető";
+    }   
+    else {
+            div.innerText = products[0].name + "nem elérhető";
+        }
+        document.body.append(div);
+
+    let special = customer === "Ádám";
+    if (!available && special) {
+        div.innerText = div.innerText + " ( de neki mégis )";
+    }
 }
+
 
 window.addEventListener("load", inventory);
